81da89f534ceeefce545eafb0035bab1dda0fdaf
HadiKia <hadikiamarzi@protonmail.com>
2024-01-27 16:09:43 +0330

    Creating services for managing APIs

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
 
 import TableCoin from "../modules/TableCoin";
+import { getCoinList } from "../../services/cryptoApi";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
 
   useEffect(() => {
-    fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&x_cg_demo_api_key=CG-EYjdgv3aUinMb5XbrmEH8nVj"
-    )
-      .then((res) => res.json())
-      .then((json) => setCoins(json));
+    const getData = async () => {
+      const res = await fetch(getCoinList());
+      const json = await res.json();
+      setCoins(json);
+    };
+
+    getData();
   }, []);
 
   return (
