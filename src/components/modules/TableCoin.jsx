3f58443f13809fa8042dfbee9383559f54b4a951
HadiKia <hadikiamarzi@protonmail.com>
2024-01-28 17:53:30 +0330

    Show coins in the TableCoins.jsx

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -1,6 +1,42 @@
+import chartUp from "../../assets/chart-up.svg"
+import chartDown from "../../assets/chart-down.svg"
+
 function TableCoin({ coins }) {
   console.log(coins);
-  return <div>TableCoin</div>;
+  return <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Coin</th>
+          <th>Name</th>
+          <th>Price</th>
+          <th>24H</th>
+          <th>Total Volume</th>
+          <th></th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {
+          coins.map((coin) => (
+            <tr key={coin.id}>
+              <td>
+                <div>
+                  <img src={coin.image} alt="" />
+                  <span>{coin.symbol.toUpperCase()}</span>
+                </div>
+              </td>
+              <td>{coin.name}</td>
+              <td>${coin.current_price.toLocaleString()}</td>
+              <td>{coin.price_change_percentage_24h > 0 ? <span>+</span> : ''}{coin.price_change_percentage_24h.toFixed(2)}%</td>
+              <td>${coin.total_volume.toLocaleString()}</td>
+              <td><img src={coin.price_change_percentage_24h > 0 ? chartUp : chartDown} alt={coin.name} /></td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
+  </div>;
 }
 
 export default TableCoin;
