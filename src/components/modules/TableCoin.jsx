5040334917cf41e0855bc1f1a23cd38231ff1afa
HadiKia <hadikiamarzi@protonmail.com>
2024-01-28 18:03:58 +0330

    Extract the TableRow component

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -1,42 +1,61 @@
-import chartUp from "../../assets/chart-up.svg"
-import chartDown from "../../assets/chart-down.svg"
+import chartUp from "../../assets/chart-up.svg";
+import chartDown from "../../assets/chart-down.svg";
 
 function TableCoin({ coins }) {
-  console.log(coins);
-  return <div>
-    <table>
-      <thead>
-        <tr>
-          <th>Coin</th>
-          <th>Name</th>
-          <th>Price</th>
-          <th>24H</th>
-          <th>Total Volume</th>
-          <th></th>
-        </tr>
-      </thead>
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Coin</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>24H</th>
+            <th>Total Volume</th>
+            <th></th>
+          </tr>
+        </thead>
 
-      <tbody>
-        {
-          coins.map((coin) => (
-            <tr key={coin.id}>
-              <td>
-                <div>
-                  <img src={coin.image} alt="" />
-                  <span>{coin.symbol.toUpperCase()}</span>
-                </div>
-              </td>
-              <td>{coin.name}</td>
-              <td>${coin.current_price.toLocaleString()}</td>
-              <td>{coin.price_change_percentage_24h > 0 ? <span>+</span> : ''}{coin.price_change_percentage_24h.toFixed(2)}%</td>
-              <td>${coin.total_volume.toLocaleString()}</td>
-              <td><img src={coin.price_change_percentage_24h > 0 ? chartUp : chartDown} alt={coin.name} /></td>
-            </tr>
-          ))
-        }
-      </tbody>
-    </table>
-  </div>;
+        <tbody>
+          {coins.map((coin) => (
+            <TableRow coin={coin} key={coin.id} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default TableCoin;
+
+const TableRow = ({
+  coin: {
+    image,
+    symbol,
+    name,
+    current_price,
+    price_change_percentage_24h: price_change,
+    total_volume,
+  },
+}) => {
+  return (
+    <tr>
+      <td>
+        <div>
+          <img src={image} alt="" />
+          <span>{symbol.toUpperCase()}</span>
+        </div>
+      </td>
+      <td>{name}</td>
+      <td>${current_price.toLocaleString()}</td>
+      <td>
+        {price_change > 0 ? <span>+</span> : ""}
+        {price_change.toFixed(2)}%
+      </td>
+      <td>${total_volume.toLocaleString()}</td>
+      <td>
+        <img src={price_change > 0 ? chartUp : chartDown} alt={name} />
+      </td>
+    </tr>
+  );
+};
