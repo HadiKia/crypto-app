886bf8f466ae219d85a2e45f8d61ec2667c5cc85
HadiKia <hadikiamarzi@protonmail.com>
2024-02-06 18:43:47 +0330

    Chart styles updated

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -10,15 +10,15 @@ import {
   YAxis,
 } from "recharts";
 
-import { convertData } from "../../helpers/convertData";
+import { convertData, formatPrice } from "../../helpers/convertData";
 
 // styles
 const containerStyle =
   "fixed inset-0 w-full h-full backdrop-blur-sm grid place-items-center";
 const backgroundDivStyle = "absolute top-0 right-0 left-0 bottom-0";
 const chartDivStyle =
-  "relative w-[800px] h-[400px] bg-[#08080899] border border-[#232530]  text-[#9295A6] rounded-lg";
-const graphStyle = "w-[760px] h-[300px]";
+  "relative w-[800px] h-[400px] bg-[#08080899] border border-[#232530] text-[#9295A6] rounded-lg p-10";
+const graphStyle = "w-full h-full";
 
 function Chart({ chart, setChart }) {
   const [type, setType] = useState("prices");
@@ -41,17 +41,21 @@ const ChartComponent = ({ data, type }) => {
   return (
     <ResponsiveContainer width="100%" height="100%">
       <LineChart width={400} height={400} data={data}>
-        {/* <CartesianGrid stroke="#9295A6" /> */}
         <Line
           type="monotone"
           dataKey={type}
           stroke="#5367FF"
           strokeWidth="2px"
         />
-        <YAxis dataKey={type} domain={["auto", "auto"]} />
+        <YAxis
+          dataKey={type}
+          tickFormatter={formatPrice}
+          domain={["auto", "auto"]}
+        />
         <XAxis dataKey="date" domain={["auto", "auto"]} />
         <Legend />
-        <Tooltip />
+        <Tooltip cursor={{ stroke: "#5367FF", strokeWidth: 0.5 }} />
+        <CartesianGrid stroke="#9295A630" />
       </LineChart>
     </ResponsiveContainer>
   );
--- src/helpers/convertData.js
+++ src/helpers/convertData.js
@@ -1,12 +1,23 @@
+const formatUnixTimestamp = (timestamp) => {
+  const date = new Date(timestamp);
+  const formattedDate = date.toLocaleDateString();
+
+  return formattedDate;
+};
+
+const formatPrice = (value) => {
+  return `$${value.toLocaleString()}`;
+};
+
 const convertData = (data, type) => {
   const convertedData = data[type].map((item) => {
     return {
-      date: item[0],
-      [type]: item[1],
+      date: formatUnixTimestamp(item[0]),
+      [type]: (item[1]),
     };
   });
 
   return convertedData;
 };
 
-export { convertData };
+export { convertData, formatPrice };
